5c523f01a6a77228f86f00fd99dc4292110e103a
MoON21 <voelknermoritz@outlook.de>
2022-11-23 02:08:36 +0100

    show blade html

---
+++ resources/views/foods/show.blade.php
@@ -0,0 +1,29 @@
+@extends('foods.layout')
+  
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Lebensmittel anzeigen</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('foods.index') }}"> Zurück</a>
+            </div>
+        </div>
+    </div>
+   
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name:</strong>
+                {{ $food->name }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Details</strong>
+                {{ $food->detail }}
+            </div>
+        </div>
+    </div>
+@endsection
